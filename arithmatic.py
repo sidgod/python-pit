613824d1c89cc491678589e105e9132616ecb7c0
Siddharth Godbole <siddharth.god@gmail.com>
2017-04-01 14:48:54 +0530

    Basic Arithmatic

--- arithmatic.py
+++ arithmatic.py
@@ -1,5 +1,6 @@
 # Health Potion Game
 import random
+import math
 
 health = 50
 
@@ -10,3 +11,28 @@ potion_health = int(random.randint(25, 50) / difficulty)
 health += potion_health
 
 print(health)
+
+# Other basic math stuff
+print(round(2.1))
+print(round(1.5))
+print(round(1.51, 1))
+print(round(1.55, 1))
+
+print(math.floor(3.9))
+print(math.ceil(2.1))
+print(math.pi)
+print(math.e)
+
+# resultant vectors
+print(math.hypot(3, 4))
+
+print(math.pow(2, 3))
+print(2 ** 3)
+
+print(math.exp(1))
+
+print(math.log(math.e))
+
+print(math.log10(1000))
+
+print(math.log2(8))
