a86a19b3d3b22bd121465342da50a0ec6c0a5a37
Siddharth Godbole <siddharth.god@gmail.com>
2017-04-01 15:16:08 +0530

    Basic Strings

---
+++ hello.py
@@ -0,0 +1,8 @@
+name = input("What's your name ? ")
+age = input("What's your age ? ")
+city = input("What's your city ? ")
+activity = input("What do you enjoy doing ? ")
+
+string = "Your name is {}, you are {} years old, you live in {} and love doing {}".format(name, age, city, activity)
+
+print(string)
---
+++ strings.py
@@ -0,0 +1,17 @@
+name = "sidgod"
+
+print(type(name))
+
+# Notice single quote to encapsulate double quote
+message = 'Say "Hellow World"'
+print(message)
+
+paragraph = """
+First line
+Second line
+Third line
+"""
+print(paragraph)
+
+hello = "Hello World!"
+print(hello)
